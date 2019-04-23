881636f9621ff709fe86e4dada66679f29913f28
AmarachiOE <ao.ejiawoko@gmail.com>
2019-04-23 11:59:37 -0700

    GET request for /api/posts endpoint

--- data/posts-router.js
+++ data/posts-router.js
@@ -1,37 +1,33 @@
 const express = require("express");
 
-const Posts = require("./db");
+const posts = require("./db");
 
 const router = express.Router();
 
 // url begins with /api/posts
 
-// GET ==================
-router.get("/", (res, req) => {
-
+// GET ALL POSTS ==================
+router.get("/", (req, res) => {
+  posts
+    .find()
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(err => {
+      res.status(500).json({ error: "The posts information could not be retrieved." });
+    });
 });
 
-// GET BY ID ==================
-router.get("/:id", (res, req) => {
-
-});
+// GET POST BY ID ==================
+router.get("/:id", (req, res) => {});
 
 // POST ==================
-router.post("/", (res, req) => {
-
-});
-
+router.post("/", (req, res) => {});
 
 // DELETE ==================
-router.delete("/:id", (res, req) => {
-
-});
-
+router.delete("/:id", (req, res) => {});
 
 // PUT ==================
-router.get("/:id", (res, req) => {
-
-});
-
+router.get("/:id", (req, res) => {});
 
 module.exports = router;
